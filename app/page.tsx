6edbd06f026de5d0cc86dff3e43b128ad8ac4a8d
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2025-03-19 21:53:23 +0200

    fix errors

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <main className="flex min-h-screen w-full flex-col items-center">
         <div className="flex w-full flex-1 flex-col items-center gap-10 md:gap-20">
           <NavBar />
-          <div className="flex flex-col gap-10 md:gap-20 px-5">
-            <div className="container flex flex-col items-center gap-16" id="hero">
+          <div className="flex flex-col gap-10 md:gap-20">
+            <div className="container flex flex-col items-center gap-16 px-5" id="hero">
               <div className="mx-auto space-y-6 lg:max-w-3xl">
                 <h1 className="mx-auto text-center text-3xl md:text-5xl font-[600]">
                   Extract <span className="text-neon">Favicons</span> Instantly!
@@ -27,7 +27,7 @@ export default function Home() {
 
               </div>
             </div>
-            <section id="benefits" className="container pt-16 md:w-full md:pt-24">
+            <section id="benefits" className="container pt-16 md:w-full md:pt-24 px-5">
               <div className="space-y-12">
                 <div className="space-y-4 text-center">
                   <p className="text-sm font-medium uppercase tracking-wider text-neon">BENEFITS</p>
@@ -94,7 +94,7 @@ export default function Home() {
                 </div>
               </div>
             </section>
-            <section id="marquee" className="max-w-[300px] sm:container mx-auto w-screen px-4 pt-16 md:w-full md:pt-24">
+            <section id="marquee" className="container sm:container mx-auto w-screen px-5 pt-16 md:w-full md:pt-24">
 
               <div className="space-y-12">
                 <div className="space-y-4 text-center">
@@ -109,8 +109,8 @@ export default function Home() {
             </section>
           </div>
         </div>
-        <footer className="container mt-10 flex w-full gap-8 border-t p-4 text-center text-xs md:items-center lg:justify-center">
-          <div className="flex flex-col items-start gap-1 text-sm lg:flex-row lg:items-center xl:gap-1">
+        <footer className="container mt-10 flex w-full gap-8 border-t p-4 text-center text-xs items-center justify-center">
+          <div className="flex flex-col gap-1 text-sm lg:flex-row items-center xl:gap-1">
             <Link className="group inline-flex h-8 w-max items-center justify-center rounded-sm bg-background px-2.5 py-2 text-sm font-medium text-foreground/70 transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50" href="mailto:khoadinh.work@gmail.com">Support: khoadinh.work@gmail.com</Link>
           </div>
 
